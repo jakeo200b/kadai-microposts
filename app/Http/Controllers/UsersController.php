9addb2f5569440bd36c63c950f1a994e35f6b1b5
ns.choi <jakeo200b@gmail.com>
2019-11-17 12:47:36 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,7 +25,7 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::find($id);
-        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(19);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
         
         $data = [
             'user'=> $user,
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,7 @@
     @if (Auth::check())
         <div class="row">
             <aside class="col-sm-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
-                    </div>
-                    <div class="card-body">
-                        <img class="rounded img-fluid" src="{{ Gravatar::src(Auth::user()->email, 500) }}" alt="">
-                    </div>
-                </div>
+                @include('users.card', ['user'=>Auth::user()])
             </aside>
             <div class="col-sm-8">
                 @if (Auth::id() == $user->id)
@@ -29,10 +22,10 @@
         </div>
     @else
         <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
-            {!! link_to_route('signup.get','Sign up now!',[],['class'=>'btn btn-lg btn-primary']) !!}
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get','Sign up now!',[],['class'=>'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
     @endif
 @endsection
